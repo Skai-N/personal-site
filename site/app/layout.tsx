0ca1f7a7b2e025da6360e84f9ad2f50c651f3163
Skai Nzeuton <san82@cornell.edu>
2026-04-15 04:19:55 -0400

    fix favicon path

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { basePath } from "@/lib/config";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,7 @@ export const metadata: Metadata = {
     type: "website",
   },
   icons: {
-    icon: "/headshot.jpg",
+    icon: `${basePath}/headshot.jpg`,
   },
 };
 
